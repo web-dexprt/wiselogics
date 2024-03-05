f553b34b50e21c56b5969c07e19d4a68d82eed13
web-dexprt <web@dexprt.com>
2024-03-05 03:14:02 -0800

    Header-logo updated

--- client/src/partials/Header.jsx
+++ client/src/partials/Header.jsx
@@ -117,8 +117,13 @@ function Header() {
               <div className="">
                 <img
                   className="h-16 w-auto object-cover object-center"
-                  src="src\images\wiselogicslogo.jpeg"
-                  alt="wiselogicslogo"
+                  // src="src\images\wiselogicslogo.jpeg"
+                  src={`${
+                    !top
+                      ? "src\images\wiselogicslogo-bg-white.jpeg"
+                      : "src\images\wiselogicslogo.jpeg"
+                  }`}
+                  alt="wiselogicslogo-logo"
                 />
               </div>
             </Link>
@@ -187,28 +192,7 @@ function Header() {
                 </Link>
               </li>
 
-              <li>
-                <Link
-                  to="/signin"
-                  className={` ${
-                    !top
-                      ? "font-medium text-black hover:text-black px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                      : "font-medium text-white hover:text-gray-100 px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                  }`}
-                >
-                  <span>Sign In</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </Link>
-              </li>
+             
             </ul>
           </nav>
         </div>
