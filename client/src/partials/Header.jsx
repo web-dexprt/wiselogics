6ce80806273e1898c293ad3476b35c0373b27d5e
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-03-06 13:40:03 +0530

    Update Header component styling

--- client/src/partials/Header.jsx
+++ client/src/partials/Header.jsx
@@ -103,7 +103,7 @@ function Header() {
 
   return (
     <header
-      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+      className={`fixed top-0 left-0 w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
         !top && "bg-white backdrop-blur-sm shadow-lg "
       }`}
     >
