e34302edb0583bce457a437da9240bb8a07cf786
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-03-06 13:46:15 +0530

    Update index.css and Footer.jsx

--- client/src/partials/Footer.jsx
+++ client/src/partials/Footer.jsx
@@ -6,8 +6,8 @@ import {FaInstagram, FaFacebook ,FaXTwitter ,FaPinterest   } from "react-icons/f
 
 function Footer() {
   return (
-    <footer>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+    <footer className="min-w-screen">
+      <div className="w-auto mx-auto px-4 sm:px-6">
         {/* Top area: Blocks */}
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
           {/* 1st block */}
